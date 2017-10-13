150c1d58277ed191cb0d7b083926ab8adefa92b2
Said Maadan <me@saidmaadan.com>
2017-10-13 10:51:54 -0500

    Updated spec to verify owner sees edit and delete links

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "Showing an article" do
     expect(current_path).to eq(article_path(@article))
 
     expect(page).not_to have_link("Edit Article")
-    expect(page).not_to have_link("Delet Article")
+    expect(page).not_to have_link("Delete Article")
   end
 
   scenario "to non-owner hide the Edit and Delete buttons" do
@@ -30,6 +30,19 @@ RSpec.feature "Showing an article" do
     expect(current_path).to eq(article_path(@article))
 
     expect(page).not_to have_link("Edit Article")
-    expect(page).not_to have_link("Delet Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A signed in owner sees both the Edit and Delete buttons" do
+    login_as(@said)
+    visit '/'
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 end
