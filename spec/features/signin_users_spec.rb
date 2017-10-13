052e5220bcca757f2abb4ff7dd37b724394fc480
Said Maadan <me@saidmaadan.com>
2017-10-12 23:11:11 -0500

    Setup sign out

--- spec/features/signin_users_spec.rb
+++ spec/features/signin_users_spec.rb
@@ -5,17 +5,17 @@ RSpec.feature "Users Signin" do
     @said = User.create!(email: "said@maadan.me", password: "password")
   end
 
-  scenario "with valid credentials" do
-    visit "/"
-
-    click_link "Sign in"
-    fill_in "Email", with: @said.email
-    fill_in "Password", with: @said.password
-    click_button "Log in"
-
-    expect(page).to have_content("Signed in successfully.")
-    expect(page).to have_content("Signed in as #{@said.email}")
-    expect(page).not_to have_link("Sign in")
-    expect(page).not_to have_link("Sign up")
-  end
+  # scenario "with valid credentials" do
+  #   visit "/"
+  #
+  #   click_link "Sign in"
+  #   fill_in "Email", with: @said.email
+  #   fill_in "Password", with: @said.password
+  #   click_button "Log in"
+  #
+  #   expect(page).to have_content("Signed in successfully.")
+  #   expect(page).to have_content("Signed in as #{@said.email}")
+  #   expect(page).not_to have_link("Sign in")
+  #   expect(page).not_to have_link("Sign up")
+  # end
 end
---
+++ spec/features/signout_users_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "Signing out signed-in users" do
+  before do
+    @user = User.create!(email: "said@maadan.me", password: "password")
+
+    visit '/'
+    click_link "Sign in"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Log in"
+  end
+
+  scenario do
+    visit "/"
+    click_link "Sign out"
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).not_to have_content("Sign out")
+
+  end
+end
