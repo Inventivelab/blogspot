9faca3821ee59c93595b11eb0aafcc2e5f68e5c6
Said Maadan <me@saidmaadan.com>
2017-10-13 10:38:53 -0500

    Show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,11 +2,38 @@ require "rails_helper"
 
 RSpec.feature "Listing Article" do
   before do
-    said = User.create(email: "said@maadan.me", password: "password")
+    @said = User.create(email: "said@maadan.me", password: "password")
 
-    @article1 = Article.create(title: "The first Post item", body: "This is the first post", user: said)
-    @article2 = Article.create(title: "The first Post item1", body: "This is the second post", user: said)
+    @article1 = Article.create(title: "The first Post item", body: "This is the first post", user: @said)
+    @article2 = Article.create(title: "The first Post item1", body: "This is the second post", user: @said)
   end
+  scenario "with articles created and user not signed in" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as(@said)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
+  end
+
   scenario " A user lists all articles" do
     visit "/"
 
@@ -18,6 +45,7 @@ RSpec.feature "Listing Article" do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
 
   scenario " A user has no articles" do
     Article.delete_all
